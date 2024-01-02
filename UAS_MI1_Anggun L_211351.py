2f9f0a0b38291feecdd6db886653f2c7296e8a17
AnggunUwU <149172875+AnggunUwU@users.noreply.github.com>
2024-01-03 02:27:47 +0700

    Update UAS_MI1_Anggun L_211351.py

--- UAS_MI1_Anggun L_211351.py
+++ UAS_MI1_Anggun L_211351.py
@@ -1,1 +1,49 @@
 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+df = pd.read_excel('hasil_cluster_rfm.xlsx')
+df = df.drop(['cluster','R_Quartile','F_Quartile','M_Quartile','RFMScore','Total_score'],axis=1)
+
+# Header Interface
+st.header("Isi dataset")
+st.write(df)
+
+# Create the slider for selecting the number of clusters (K)
+st.sidebar.subheader("Nilai jumlah K")
+clust = st.sidebar.slider("Pilih jumlah cluster: ", 2, 10, 1, 1)
+
+# Scatter plot function
+def k_means(n_clust):
+    kmean = KMeans(n_clusters=n_clust).fit(df)
+    df['Labels'] = kmean.labels_
+
+    st.subheader('Cluster Plot')
+    fig, axes = plt.subplots(3,1, figsize=(20, 40))
+
+    sns.scatterplot(x=df['Amount'], y=df['Recency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=axes[0])
+    axes[0].set_title('Amount vs Recency')
+
+    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=axes[1])
+    axes[1].set_title('Amount vs Frequency')
+
+    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Recency'], ax=axes[2])
+    axes[2].set_title('Amount vs Frequency vs Recency')
+
+    st.pyplot(fig)
+
+# Clustering process and scatter plot
+if st.button("Proses Klasterisasi dan Tampilkan Plot"):
+    clusters = []
+    for i in range(1, 11):
+        km = KMeans(n_clusters=i).fit(df)
+        clusters.append(km.inertia_)
+
+    # Display the elbow plot
+    st.subheader("Mencari Elbow")
+    st.line_chart(clusters, use_container_width=True)
+
+    k_means(clust)
